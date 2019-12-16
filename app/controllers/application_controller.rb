db7f6406c829a5a2275072cb41350aa540e7562a
Sunny-arren <cysctoqv25lp@gmail.com>
2019-12-16 15:13:49 +0900

    ベーシック認証の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  http_basic_authenticate_with :name => ENV['enlight'], :password => ENV['11110'] if Rails.env == "production"
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
