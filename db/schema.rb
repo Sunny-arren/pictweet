263581fe7e1666830cdbe9861de8b140672a896d
Sunny-arren <cysctoqv25lp@gmail.com>
2019-12-16 13:42:13 +0900

    schemaファイルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_082159) do
+ActiveRecord::Schema.define(version: 2019_10_14_074941) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -21,11 +21,11 @@ ActiveRecord::Schema.define(version: 2019_10_14_082159) do
   end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "image"
     t.text "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer "user_id"
+    t.text "image"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
